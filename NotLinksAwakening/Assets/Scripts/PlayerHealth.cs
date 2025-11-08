00d5a16b7d5f671ba38f84f3b3664e430ef38f45
Jonathan Gerlach <jg.gerlach4@gmail.com>
2025-11-08 15:40:03 -0500

    More stuff

--- NotLinksAwakening/Assets/Scripts/PlayerHealth.cs
+++ NotLinksAwakening/Assets/Scripts/PlayerHealth.cs
@@ -3,23 +3,37 @@ using UnityEngine;
 // Example PlayerHealth script(attach to player GameObject)
     public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] private int maxHealth = 100;
-    private int currentHealth;
+    [SerializeField] private float maxHealth = 100;
+    [SerializeField] private HealthBar healthBar;
+    private float currentHealth;
 
     void Start()
     {
         currentHealth = maxHealth;
+        if (healthBar != null)
+        { 
+            healthBar.SetMaxHealth(maxHealth);
+        }
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
         Debug.Log($"Player took {damage} damage. Current health: {currentHealth}");
-
+        if (healthBar != null)
+        {
+            healthBar.SetHealth(currentHealth);
+        }
         if (currentHealth <= 0)
         {
             Die();
         }
+    }
+
+    public float GetCurrentHealth()
+    {
+        return currentHealth;
     }
 
     void Die()
